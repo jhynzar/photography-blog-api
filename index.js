a52ceca9a43b8e16b3d2670077e2b8ab61ee573e
jhynzar <jhynzar@gmail.com>
2019-03-15 10:51:24 +0800

    [TEST] CORS Policy

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ const PORT = process.env.PORT || 5000;
  */
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 /**
  * API
  */
