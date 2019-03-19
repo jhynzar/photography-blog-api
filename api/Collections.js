b351ce75d97936523fd2f430d18b5a0802a39faf
jhynzar <jhynzar@gmail.com>
2019-03-19 08:33:20 +0800

    [API] Added gallery links to collections

--- api/Collections.js
+++ api/Collections.js
@@ -4,32 +4,38 @@ const collections = [
     {
         title: 'The First Collection',
         coverImg: 'img1.jpg',
-        folderName: 'first-collection'
+        folderName: 'first-collection',
+        link: '/single'
     },
     {
         title: 'Is this the 2nd collection?',
         coverImg: 'img2.jpg',
-        folderName: 'second-collection'
+        folderName: 'second-collection',
+        link: '/single'
     },
     {
         title: 'oops 3rd',
         coverImg: 'img3.jpg',
-        folderName: 'third-collection'
+        folderName: 'third-collection',
+        link: '/single'
     },
     {
         title: 'hahaha last.',
         coverImg: 'img4.jpg',
-        folderName: 'fourth-collection'
+        folderName: 'fourth-collection',
+        link: '/single'
     },
     {
         title: 'Same Cover',
         coverImg: 'img4.jpg',
-        folderName: 'fourth-collection'
+        folderName: 'fourth-collection',
+        link: '/single'
     },
     {
         title: 'ALT IMAGE',
         coverImg: 'img5.jpg',
-        folderName: 'fourth-collection'
+        folderName: 'fourth-collection',
+        link: '/single'
     },
 ];
 
